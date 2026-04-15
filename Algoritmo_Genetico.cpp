71b223d5d5c0de66c3608f699ce9439fe6e240f6
Sawed304 <113204232+Sawed304@users.noreply.github.com>
2026-04-14 23:33:20 -0500

    Update Algoritmo_Genetico.cpp

--- Algoritmo_Genetico.cpp
+++ Algoritmo_Genetico.cpp
@@ -3,7 +3,10 @@
 #include <vector>
 #include <bitset>
 
-#define BitsX 5
+//Cantidad de Bits del rango en X
+#define BitsX 5 
+
+//Cantidad de Bits del rango en Y
 #define BitsY 6
 
 using namespace std;
@@ -16,43 +19,73 @@ double f(int x, int y) {
     return result;
 }
 
-//principal
-void Algoritmo_Genetico(int CantIn, int GenId, vector<int> px, vector<int> py) {
-
-    vector< bitset<BitsX> > Px;
-    vector< bitset<BitsY> > Py;
-
-    //Población "x" y "y" guardados en bits
-    for (int i : px) {
-        Px.push_back(bitset<BitsX>(i));
+//Funcion escoger Maximo o el Minimo
+double Max0Min(vector<double> val, bool i) {
+    double result{val[0]};
+    for (auto j : val) {
+        if (i) {
+            if (j > result) result = j;
+        }
+        else { if (j < result) result = j; }
     }
-    for (int i : py) {
-        Py.push_back(bitset<BitsY>(i));
-    }
+    return result;
+}
 
-    vector<int> vx = px;
-    vector<int> vy = py;
+//Funcion escoger la siguiente población
+pair < vector< bitset<BitsX> >, vector< bitset<BitsY> > > Sig() {
+
+}
+
+
+/*Funcion Principal
+
+CantIn : Número de individuos
+GenId : Identificador de la generación
+px : Vector de población en X en bits
+py : Vector de población en Y en bits
+MaxOrMin : Bool que decide si es Maximizar(1) o Minimizar(0)
+
+*/
+void Algoritmo_Genetico(int CantIn, int GenId, vector< bitset<BitsX> > px, vector< bitset<BitsY> > py, bool MaxOrMin) {
+
+    vector<int> Vx; //Vx: vector de valores en INT de X
+    vector<int> Vy; //Vy: vector de valores en INT de Y
+
+    vector< bitset<BitsX> > Px = px; // Px: vector de valores en bits de X
+    vector< bitset<BitsY> > Py = py; // Px: vector de valores en bits de Y
 
     //Función cruzamiento
     if(GenId != 0){
     
     }
 
-    vector<double> fucn;
-    vector<double> fucn_Sum;
-    vector<double> Ve;
-    vector<int> Va;
+    //Transformacion de bits a int
+    for (auto i : Px) {
+        Vx.push_back(i.to_ulong()); // "to_ulong()" transforma directamente los bits a enteros
+    }
+    for (auto i : Py) {
+        Vy.push_back(i.to_ulong());
+    }
+
+
+
+    vector<double> fucn; // fucn: vector de resultados de f(Xi,Yi)
+    vector<double> fucn_Sum; // fucn_Sum: vector de resultados de f(Xi,Yi) / Sumatoria
+    vector<double> Ve; // Ve: vector de valores de Valor Esperado
+    vector<int> Va; // Va: vector de valores de Valor Actual
 
     double sumatoria{0};
     double media{0};
+    double Max_Min{0};
 
     //Columna Funcion
     for (int i = 0; i < CantIn; i++) {
-        double r = f(vx[i], vy[i]);
+        double r = f(Vx[i], Vy[i]);
         fucn.push_back(r);
         sumatoria += r;
     }
 
+    Max_Min = Max0Min(fucn, MaxOrMin);
     media = sumatoria / CantIn;
 
     //Columna Funcion / Sumatoria
@@ -69,6 +102,8 @@ void Algoritmo_Genetico(int CantIn, int GenId, vector<int> px, vector<int> py) {
     for (auto i : Ve) {
         Va.push_back( round(i) ); // "round" redondea
     }
+
+    pair<vector< bitset<BitsX> >, vector< bitset<BitsY> >> NPoblacion; //Siguiente población en "X" y "Y"
 
 
 
